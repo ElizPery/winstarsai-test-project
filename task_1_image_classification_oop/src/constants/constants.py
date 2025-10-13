ba47f9d520e0f551ea203b78c7442c0c0e9f89e2
Elizabeth Pereriadkina <eli@Mac.fritz.box>
2025-10-13 17:26:00 +0100

    edit path to RandomForestClassifierModel

--- task_1_image_classification_oop/src/constants/constants.py
+++ task_1_image_classification_oop/src/constants/constants.py
@@ -1,4 +1,4 @@
-from task_1_image_classification_oop.src.utils.classes.random_forest_classifier_model import RandomForestClassifierModel
+from src.utils.classes.random_forest_classifier_model import RandomForestClassifierModel
 from src.utils.classes.feed_forward_nn_classifier import FeedForwardNNClassifier
 from src.utils.classes.cnn_classifier import CNNClassifier
 
